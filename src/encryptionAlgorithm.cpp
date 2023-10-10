c2a51712f5f3b24caf64a453fd29dbd1dccc974b
nsoliven <zurzie2@gmail.com>
2023-10-09 21:27:29 -0700

    changes in encryption algorithm

--- src/encryptionAlgorithm.cpp
+++ src/encryptionAlgorithm.cpp
@@ -6,3 +6,10 @@ Created in 2023 by NSOLIVEN
 
 */
 #include "encryptionAlgorithm.h"
+
+string Encryption :: aes_encrypt(string key, string password){
+    AES_KEY aesKey;
+}
+string Encryption :: aes_decrypt(string key, string password){
+
+}
