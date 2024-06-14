800abdadcd94b7202ecfbb60667dd4bad3fbb4ee
nsoliven <zurzie2@gmail.com>
2024-06-13 22:30:03 -0700

    feat: start of KDF function building

--- include/encryptionAlgorithm.h
+++ include/encryptionAlgorithm.h
@@ -8,6 +8,7 @@ Created in 2023 by NSOLIVEN
 #ifndef ENCRYPTION_H
 #define ENCRYPTION_H
 
+#include <botan/kdf.h>
 #include <botan/hash.h>
 #include <botan/hex.h>
 #include <string>
@@ -22,13 +23,16 @@ Complete WIP!
 */
 
 class Encryption {
+private:
+    const size_t KDF_ITERATIONS = 60000;
 public:
     // Define the AES encryption and decryption functions
-    std::string aes_encrypt(const std::string plainText, const std::string key);
-    std::string aes_decrypt(const std::string cipherText, const std::string key);
-    std::string deriveKey(const std::string masterPassword);
+    std::string aes_encrypt(const std::string &plainText, const std::string &key);
+    std::string aes_decrypt(const std::string &cipherText, const std::string &key);
+    std::string deriveKey(const std::string &masterPassword);
+    std::string generateSalt(const int &saltLength);
+    std::string hashAndSalt(const std::string &strToHash, const std::string &salt);
 
-    
 };
 
 #endif
--- src/encryptionAlgorithm.cpp
+++ src/encryptionAlgorithm.cpp
@@ -26,7 +26,7 @@ Created in 2023 by NSOLIVEN
  * @param string key to use to encrypt
  * @return string of password encrypted item to store in database later on
  */
-std::string Encryption::aes_encrypt(const std::string plainText, const std::string key){
+std::string Encryption::aes_encrypt(const std::string &plainText, const std::string &key){
     std::string encryptedStr;
     
     return encryptedStr;
@@ -39,7 +39,7 @@ std::string Encryption::aes_encrypt(const std::string plainText, const std::stri
  * @param string key
  * @return string of password unencrypted
  */
-std::string Encryption::aes_decrypt(const std::string cipherText, const std::string key){
+std::string Encryption::aes_decrypt(const std::string &cipherText, const std::string &key){
     std::string decryptedStr;
     return decryptedStr;
 }
@@ -50,7 +50,23 @@ std::string Encryption::aes_decrypt(const std::string cipherText, const std::str
  * @param string masterPassword
  * @return key derived from masterPassword
  */
-std::string Encryption::deriveKey(const std::string masterPassword){
+std::string Encryption::deriveKey(const std::string &masterPassword){
     std::string key;
     return key;
+}
+
+std::string Encryption::generateSalt(const int &saltLength){
+
+
+/**
+ * @brief returns a hashed string after passing a salt and str to hash
+ *  
+ * @param string string to hash
+ * @param string salt 
+ * @return hashed string
+ */
+}
+std::string Encryption::hashAndSalt(const std::string &strToHash, const std::string &salt){
+    const size_t iterations = KDF_ITERATIONS;
+    std::unique_ptr<Botan::KDF> kdf(Botan::KDF::create("PBKDF2(SHA-256," + std::to_string(KDF_ITERATIONS) + ")"));
 }
